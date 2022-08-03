a8aa3d0b7e5109b88a2b8170a2592855c8d442a1
sko1208 <skosam1208@gmail.com>
2022-08-03 17:06:52 +0300

    maked responsive menu, debug icons, maked burger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,60 +1,28 @@
 "use strict";
 // render header menu
-const wrapperBlock = `
-<div class="container">
-    <div class="header__logo">
-        <a href="../index.html"><img src="img/logo.png" alt="logo"></a>
-    </div>
-    <ul class="header__menu">
-        <li><a href="../index.html">Main</a></li>
-        <li><a href="../pages/gallary.html">Gallery</a></li>
-        <li><a href="#">Exhibitions</a></li>
-        <li><a href="#">Bio</a></li>
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#" data-open>Contacts</a></li>
-        <li><a href="#">Auction</a></li>
-        <li><a href="#">NFT</a></li>
-    </ul>
-    <div class="header__like">
-        <a href="#">
-        </a>
-    </div>
-    <ul class="header__social-icons">
-        <li>
-            <a href="#"></a>
-        </li>
-        <li>
-            <a href=""></a>
-        </li>
-        <li>
-            <a href=""></a>
-        </li>
-        <li>
-            <a href=""></a>
-        </li>
-    </ul>
-</div>
-`;
+// const wrapperBlock = `
 
-class addElements {
-    constructor(parentSelector, wrapper) {
-        this.parent = document.querySelector(parentSelector);
-        this.wrapper = wrapper;
+// `;
+
+// class addElements {
+//     constructor(parentSelector, wrapper) {
+//         this.parent = document.querySelector(parentSelector);
+//         this.wrapper = wrapper;
        
-    }
-    renderHeader() {
-        const element = document.createElement('header');
-        element.classList.add('header');
-        element.innerHTML = this.wrapper;
-        this.parent.prepend(element);
-    }
-}
+//     }
+//     renderHeader() {
+//         const element = document.createElement('header');
+//         element.classList.add('header');
+//         element.innerHTML = this.wrapper;
+//         this.parent.prepend(element);
+//     }
+// }
 
 
-new addElements('body', wrapperBlock).renderHeader();
+// new addElements('body', wrapperBlock).renderHeader();
 
 
-// Modal window
+// Modal Window
 const openTrigger = document.querySelector('[data-open]'),
       modalWindow = document.querySelector('.modal'),
       closeTrigger = document.querySelector('[data-close]');
@@ -91,5 +59,13 @@ const openTrigger = document.querySelector('[data-open]'),
     });
 
 
+// Burger Menu
+const bugrerMenuOpen = document.querySelector('.hamburger'),
+      burgerMenu = document.querySelector('.header__menu_wrapper'),
+      burgerCloseButton = document.querySelector('.header__menu_close');
 
+bugrerMenuOpen.addEventListener('click', () => {
+    burgerMenu.classList.toggle('header__menu_wrapper_active');
+    bugrerMenuOpen.classList.toggle('hamburger__active');
+});
 
